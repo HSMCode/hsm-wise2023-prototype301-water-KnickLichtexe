55fca543f01867fd748069714d884fc6a2f1f5cb
Johanna <josupeitjohanna@gmail.com>
2024-01-17 14:54:46 +0100

    background loop

--- Assets/Scripts/backgroundSprites.cs
+++ Assets/Scripts/backgroundSprites.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 // this script moves the sprites in the background of the scene
 public class backgroundSprites : MonoBehaviour
 {
-   
+    void Update()
+    {
+        Camera mainCamera = Camera.main;
+        float distance = mainCamera.transform.position.x - transform.position.x;
 
+        if ( distance > 200)
+        {
+            transform.position += new Vector3(400, 0, 0);
+        }
+    }
 }
